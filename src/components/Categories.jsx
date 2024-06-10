244bea1274135c19b6fcc24c54a02b25ca27cc66
fariasagustin3 <fariasagustin3@gmail.com>
2024-06-10 11:03:47 -0300

    agregado archivo data.js y optimizado el rendimiento del renderizado de imagenes en los filtros

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,82 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './categories.css';
-
-// son todas imágenes en blanco y negro
-// categories: computadoras, perifericos, cartelera digital, audio
-const data = [
-  {
-    id: 1,
-    name: "CELULARES Y TELEFONÍA",
-    image: "/images/blanco_negro/celulares_y_telefonia.jpg",
-    category: "computadoras",
-  },
-  {
-    id: 2,
-    name: "COMPONENTES PC",
-    image: "/images/blanco_negro/componentes_pc.png",
-    category: "computadoras",
-  },
-  {
-    id: 3,
-    name: "GAMING",
-    image: "/images/blanco_negro/gaming.png",
-    category: "perifericos",
-  },
-  {
-    id: 4,
-    name: "IMPRESORAS",
-    image: "/images/blanco_negro/impresoras.png",
-    category: "perifericos",
-  },
-  {
-    id: 5,
-    name: "LICENCIAS DIGITALES",
-    image: "/images/blanco_negro/licencias_digitales.png",
-    category: "cartelera_digital",
-  },
-  {
-    id: 6,
-    name: "MANTENIMIENTO PC",
-    image: "/images/blanco_negro/mantenimiento_pc.png",
-    category: "computadoras",
-  },
-  {
-    id: 7,
-    name: "MONITORES",
-    image: "/images/blanco_negro/monitores.png",
-    category: "perifericos",
-  },
-  {
-    id: 8,
-    name: "NOTEBOOKS Y TABLETS",
-    image: "/images/blanco_negro/notebooks_y_tablets.png",
-    category: "computadoras",
-  },
-  {
-    id: 9,
-    name: "PC ESCRITORIO, MINI PC Y ALL IN ONE",
-    image: "/images/blanco_negro/pc_escritorio_mini_pc_y_all_in_one.png",
-    category: "computadoras",
-  },
-  {
-    id: 10,
-    name: "PERIFÉRICOS",
-    image: "/images/blanco_negro/perifericos.png",
-    category: "perifericos",
-  },
-  {
-    id: 11,
-    name: "SEGURIDAD ELECTRÓNICA",
-    image: "/images/blanco_negro/seguridad_electronica.png",
-    category: "perifericos",
-  },
-  {
-    id: 12,
-    name: "TONER ORIGINALES Y ALTERNATIVOS",
-    image: "/images/blanco_negro/toner_originales_y_aternativos.png",
-    category: "perifericos",
-  },
-]
+import { productos, categories } from '../data';
 
 const HoverElement = ({ id, hoverId, product }) => {
   return (
@@ -105,20 +29,16 @@ const Categories = () => {
     setShowHoverElement(null)
   }
 
-  useEffect(() => {
+  const getProductsByCategories = (category) => {
     if (selected === "todos") {
-      setProducts(data)
-    } else if (selected === "computadoras") {
-      setProducts(data.filter(product => product.category === "computadoras"))
-    } else if (selected === "perifericos") {
-      setProducts(data.filter(product => product.category === "perifericos"))
-    } else if (selected === "computadoras") {
-      setProducts(data.filter(product => product.category === "computadoras"))
-    } else if (selected === "cartelera_digital") {
-      setProducts(data.filter(product => product.category === "cartelera_digital"))
-    } else if (selected === "audio") {
-      setProducts(data.filter(product => product.category === "audio"))
+      setProducts(productos)
+    } else {
+      setProducts(productos.filter(product => product.category === category))
     }
+  }
+
+  useEffect(() => {
+    getProductsByCategories(selected)
   }, [selected])
 
   return (
@@ -132,21 +52,13 @@ const Categories = () => {
 
       {/* botones toggle */}
       <div className='w-screen flex items-center flex-wrap px-5 md:pl-24 gap-5 md:gap-10 mb-10'>
-        <button
-          onClick={() => setSelected("todos")}
-          className={selected === "todos" ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>TODOS</button>
-        <button
-          onClick={() => setSelected("computadoras")}
-          className={selected === "computadoras" ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>COMPUTADORAS</button>
-        <button
-          onClick={() => setSelected("perifericos")}
-          className={selected === "perifericos" ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>PERIFÉRICOS</button>
-        <button
-          onClick={() => setSelected("cartelera_digital")}
-          className={selected === "cartelera_digital" ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>CARTELERA DIGITAL</button>
-        <button
-          onClick={() => setSelected("audio")}
-          className={selected === "audio" ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>AUDIO</button>
+        {categories.map(category => (
+          <button
+            onClick={() => setSelected(category.value)}
+            className={selected === category.value ? 'py-5 px-3 text-sm font-semibold text-white hover:text-gray-400 transition-all duration-500 bg-[#731523] rounded-md' : "bg-transparent font-semibold hover:text-[#000] transition-all duration-500 py-5"}>
+            {category.name}
+          </button>
+        ))}
       </div>
 
       {/* listado de categorias de productos */}
@@ -162,8 +74,16 @@ const Categories = () => {
               backgroundSize: "cover"
             }}
           >
-
-            <HoverElement id={product.id} product={product} hoverId={showHoverElement} />
+            <div className='relative h-full w-full'>
+              <div className={product.id === showHoverElement ? 'h-full absolute transition-opacity duration-500 left-0 right-0 top-0 bottom-0 m-auto' : "h-full opacity-0 left-0 right-0 top-0 bottom-0 m-auto transition-opacity duration-500"}>
+                <div className='flex flex-col h-full items-center justify-center'>
+                  <h1 className='z-10 text-white opacity-1 text-sm text-center'>{product.name}</h1>
+                  <button className='z-10 bg-[#731523] text-sm px-3 py-2 font-semibold text-white opacity-1'>Consultanos</button>
+                  <div className='absolute h-full w-full bg-black opacity-80'></div>
+                </div>
+              </div>
+            </div>
+            {/* <HoverElement id={product.id} product={product} hoverId={showHoverElement} /> */}
           </div>
         ))}
       </div>
---
+++ src/data.js
@@ -0,0 +1,127 @@
+export const categories = [
+  {
+    id: 1,
+    name: "TODOS",
+    value: "todos"
+  },
+  {
+    id: 2,
+    name: "COMPUTADORAS",
+    value: "computadoras"
+  },
+  {
+    id: 3,
+    name: "NOTEBOOKS Y MONITORES",
+    value: "notebooks_y_monitores"
+  },
+  {
+    id: 4,
+    name: "TELEFONÍA",
+    value: "telefonia"
+  },
+  {
+    id: 5,
+    name: "SEGURIDAD ELECTRÓNICA",
+    value: "seguridad_electronica"
+  },
+  {
+    id: 6,
+    name: "COMPONENTES PC",
+    value: "componentes_pc"
+  },
+  {
+    id: 7,
+    name: "PERIFÉRICOS",
+    value: "perifericos"
+  },
+  {
+    id: 8,
+    name: "GAMING",
+    value: "gaming"
+  },
+  {
+    id: 9,
+    name: "IMPRESORAS",
+    value: "impresoras"
+  },
+  {
+    id: 10,
+    name: "MANTENIMIENTO PC",
+    value: "mantenimiento_pc"
+  },
+];
+
+export const productos = [
+  {
+    id: 1,
+    name: "CELULARES Y TELEFONÍA",
+    image: "/images/blanco_negro/celulares_y_telefonia.jpg",
+    category: "telefonia",
+  },
+  {
+    id: 2,
+    name: "COMPONENTES PC",
+    image: "/images/blanco_negro/componentes_pc.png",
+    category: "componentes_pc",
+  },
+  {
+    id: 3,
+    name: "GAMING",
+    image: "/images/blanco_negro/gaming.png",
+    category: "gaming",
+  },
+  {
+    id: 4,
+    name: "IMPRESORAS",
+    image: "/images/blanco_negro/impresoras.png",
+    category: "impresoras",
+  },
+  {
+    id: 5,
+    name: "LICENCIAS DIGITALES",
+    image: "/images/blanco_negro/licencias_digitales.png",
+    category: "computadoras",
+  },
+  {
+    id: 6,
+    name: "MANTENIMIENTO PC",
+    image: "/images/blanco_negro/mantenimiento_pc.png",
+    category: "mantenimiento_pc",
+  },
+  {
+    id: 7,
+    name: "MONITORES",
+    image: "/images/blanco_negro/monitores.png",
+    category: "notebooks_y_monitores",
+  },
+  {
+    id: 8,
+    name: "NOTEBOOKS Y TABLETS",
+    image: "/images/blanco_negro/notebooks_y_tablets.png",
+    category: "notebooks_y_monitores",
+  },
+  {
+    id: 9,
+    name: "PC ESCRITORIO, MINI PC Y ALL IN ONE",
+    image: "/images/blanco_negro/pc_escritorio_mini_pc_y_all_in_one.png",
+    category: "componentes_pc",
+  },
+  {
+    id: 10,
+    name: "PERIFÉRICOS",
+    image: "/images/blanco_negro/perifericos.png",
+    category: "perifericos",
+  },
+  {
+    id: 11,
+    name: "SEGURIDAD ELECTRÓNICA",
+    image: "/images/blanco_negro/seguridad_electronica.png",
+    category: "seguridad_electronica",
+  },
+  {
+    id: 12,
+    name: "TONER ORIGINALES Y ALTERNATIVOS",
+    image: "/images/blanco_negro/toner_y_tintas_originals_y_alternativos.png",
+    category: "impresoras",
+  },
+]
